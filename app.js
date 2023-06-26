86f12986a7a393ab93472f0357640e831ac7a3cf
virajrodge5 <virajrodge5@gmail.com>
2023-06-26 22:41:09 +0530

    First commit

---
+++ app.js
@@ -0,0 +1,76 @@
+//jshint esversion:6
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const _ = require("lodash");
+
+const homeStartingContent = 
+  "Are you a passionate writer, eager to share your thoughts, unleash your creativity and experiences with the world? Look no further! Our basic blog website is designed to be your go-to platform for daily blogging and to provide you with a platform to express your creativity.";
+const homeMidContent = "With our user-friendly interface, you can effortlessly create and save daily blogs that reflect your unique perspective. We understand that simplicity is key. Our streamlined design ensures that you can focus on what matters most: your writing. No distractions, no complicated features. Just a clean and intuitive interface that allows you to bring your ideas to life.";
+const homeEndContent = 
+"So, why wait? Start your daily blogging journey today and let your words resonate with others.";
+const aboutContent =
+  "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
+const contactContent =
+  "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
+
+const app = express();
+
+app.set("view engine", "ejs");
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+var posts = [];
+
+app.get("/", function (req, res) {
+  res.render("home", {
+    startingContent: homeStartingContent,
+    midContent: homeMidContent,
+    endContent: homeEndContent,
+    posts: posts,
+  });
+});
+
+app.get("/about", function (req, res) {
+  res.render("about", { aboutContent: aboutContent });
+});
+
+app.get("/contact", function (req, res) {
+  res.render("contact", { contactContent: contactContent });
+});
+
+app.get("/compose", function (req, res) {
+  res.render("compose");
+});
+
+app.post("/compose", function (req, res) {
+  const post = {
+    title: req.body.postTitle,
+    content: req.body.postBody,
+  };
+
+  posts.push(post);
+
+  res.redirect("/");
+});
+
+app.get("/posts/:postName", function (req, res) {
+  const requestedTitle = _.lowerCase(req.params.postName);
+
+  posts.forEach(function (post) {
+    const storedTitle = _.lowerCase(post.title);
+
+    if (storedTitle === requestedTitle) {
+      res.render("post", {
+        title: post.title,
+        content: post.content,
+      });
+    }
+  });
+});
+
+app.listen(3000, function () {
+  console.log("Server started on port 3000");
+});
